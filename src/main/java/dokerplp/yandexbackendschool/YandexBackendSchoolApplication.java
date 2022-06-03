1ef1660d9f8a7dd1e7c1484cb45562d1cd2b3fe9
Valerii Butorin <butorin.v.a@mail.ru>
2022-06-03 17:47:41 +0300

    init

---
+++ src/main/java/dokerplp/yandexbackendschool/YandexBackendSchoolApplication.java
@@ -0,0 +1,13 @@
+package dokerplp.yandexbackendschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexBackendSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexBackendSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dokerplp/yandexbackendschool/YandexBackendSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dokerplp.yandexbackendschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexBackendSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
