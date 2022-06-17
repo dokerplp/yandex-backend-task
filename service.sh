14e6130ef937cb98fe69838ed95228c133f083db
Valerii Butorin <butorin.v.a@mail.ru>
2022-06-17 19:05:55 +0300

    ports

--- service.sh
+++ service.sh
@@ -1,3 +1,4 @@
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo mv yandex.service /etc/systemd/system/
 sudo systemctl enable yandex
 sudo systemctl start yandex
