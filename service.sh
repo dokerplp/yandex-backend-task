a64cfd2d6eae06184057aab8b724dd4d009102f7
Valerii Butorin <butorin.v.a@mail.ru>
2022-06-17 19:18:54 +0300

    service

--- service.sh
+++ service.sh
@@ -1,4 +1,3 @@
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo mv yandex.service /etc/systemd/system/
 sudo systemctl enable yandex
 sudo systemctl start yandex
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 mvn clean install -DskipTests
 sudo docker build -t yandex-backend-task.jar .
 sudo docker-compose up
