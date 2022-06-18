d671cfaad130f53cb9dbf59ae365f6779576f121
Valerii Butorin <butorin.v.a@mail.ru>
2022-06-18 14:38:45 +0300

    service

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo mvn install -DskipTests
 sudo docker build -t yandex-backend-task.jar .
 sudo docker network prune -f
-sudo docker-compose up --remove-orphans
+sudo docker-compose up --force-recreate --remove-orphans
